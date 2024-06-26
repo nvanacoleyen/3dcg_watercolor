d313e48c843c151e2fb54044298d852a4e9b8c86
Floris van Veen <100093168+bwmfvanveen-II@users.noreply.github.com>
2024-06-26 15:20:16 +0200

    not out of bounds

--- src/move_pigment.cpp
+++ src/move_pigment.cpp
@@ -14,8 +14,8 @@ void movePigment(std::vector<Cell>* Grid, Staggered_Grid* u, Staggered_Grid* v)
 	for (float t = 0.f; t <= 1.f; t += delta_t) {
 
 		/* Loop through all cells and adjust pigments */
-		for (int j = 1; j < HEIGHT - 1; j++) {
-			for (int i = 1; i < WIDTH - 1; i++) {
+		for (int j = 1; j < HEIGHT - 2; j++) {
+			for (int i = 1; i < WIDTH - 2; i++) {
 				new_Grid[WIDTH * (j + 1) + (i + 1) + 1].m_pigmentConc += delta_t * std::max(0.f, (u->get_at_pos(i + 0.5, j) * current_Grid[WIDTH * (j + 1) + i + 1].m_pigmentConc));
 				new_Grid[WIDTH * (j + 1) + (i - 1) + 1].m_pigmentConc += delta_t * std::max(0.f, (-u->get_at_pos(i - 0.5, j) * current_Grid[WIDTH * (j + 1) + i + 1].m_pigmentConc));
 				new_Grid[WIDTH * (j + 2) + i + 1].m_pigmentConc += delta_t * std::max(0.f, (v->get_at_pos(i, j + 0.5) * current_Grid[WIDTH * (j + 1) + i + 1].m_pigmentConc));
